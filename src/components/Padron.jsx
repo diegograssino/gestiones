ca6ad3332465d909b644a412a0cbe04ba5d4aa03
Diego Grassino <diegograssino@gmail.com>
2022-07-11 02:15:55 -0300

    v2

--- src/components/Padron.jsx
+++ src/components/Padron.jsx
@@ -66,6 +66,20 @@ const Padron = () => {
   const [errorEmailPersonal, setErrorEmailPersonal] = useState(false);
   const [errorTelFijoPersonal, setErrorTelFijoPersonal] = useState(false);
   const [errorRelacionDeDependencia, setErrorRelacionDeDependencia] = useState(false);
+  const [errorEsAutonomo, setErrorEsAutonomo] = useState(false);
+  const [errorDirConsultorio1, setErrorDirConsultorio1] = useState(false);
+  const [errorFantasiaConsultrio1, setErrorFantasiaConsultrio1] = useState(false);
+  const [errorHabilitacionConsultrio1, setErrorHabilitacionConsultrio1] = useState(false);
+  const [errorDirConsultorio2, setErrorDirConsultorio2] = useState(false);
+  const [errorFantasiaConsultorio2, setErrorFantasiaConsultrio2] = useState(false);
+  const [errorHabilitacionConsultrio2, setErrorHabilitacionConsultrio2] = useState(false);
+  const [errorTelFijoLaboral, setErrorTelFijoLaboral] = useState(false);
+  const [errorTelCelularLaboral, setErrorTelCelularLaboral] = useState(false);
+  const [errorFacebook, setErrorFacebook] = useState(false);
+  const [errorInstagram, setErrorInstagram] = useState(false);
+  const [errorTwitter, setErrorTwitter] = useState(false);
+  const [errorWeb, setErrorWeb] = useState(false);
+  const [errorTikTok, setErrorTikTok] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -89,6 +103,133 @@ const Padron = () => {
       setErrorApellidos(true);
     } else {
       setErrorApellidos(false);
+    }
+    if (!validator.isNumeric(data.dni)) {
+      validationOk = false;
+      setErrorDni(true);
+    } else {
+      setErrorDni(false);
+    }
+    if (!validator.isNumeric(data.cuit)) {
+      validationOk = false;
+      setErrorCuit(true);
+    } else {
+      setErrorCuit(false);
+    }
+    if (!validator.isNumeric(data.matricula)) {
+      validationOk = false;
+      setErrorMatricula(true);
+    } else {
+      setErrorMatricula(false);
+    }
+    if (!validator.isAlpha(data.sexo)) {
+      validationOk = false;
+      setErrorSexo(true);
+    } else {
+      setErrorSexo(false);
+    }
+    if (validator.isEmpty(data.direccion)) {
+      validationOk = false;
+      setErrorDireccion(true);
+    } else {
+      setErrorDireccion(false);
+    }
+    if (!validator.isEmail(data.emailPersonal)) {
+      validationOk = false;
+      setErrorEmailPersonal(true);
+    } else {
+      setErrorEmailPersonal(false);
+    }
+    if (!validator.isNumeric(data.telFijoPersonal)) {
+      validationOk = false;
+      setErrorTelFijoPersonal(true);
+    } else {
+      setErrorTelFijoPersonal(false);
+    }
+    if (!validator.isAlpha(data.esAutonomo)) {
+      validationOk = false;
+      setErrorEsAutonomo(true);
+    } else {
+      setErrorEsAutonomo(false);
+    }
+    if (validator.isEmpty(data.dirConsultorio1)) {
+      validationOk = false;
+      setErrorDirConsultorio1(true);
+    } else {
+      setErrorDirConsultorio1(false);
+    }
+    if (validator.isEmpty(data.fantasiaConsultorio1)) {
+      validationOk = false;
+      setErrorFantasiaConsultrio1(true);
+    } else {
+      setErrorFantasiaConsultrio1(false);
+    }
+    if (!validator.isAlphanumeric(data.habilitacionConsultorio1)) {
+      validationOk = false;
+      setErrorHabilitacionConsultrio1(true);
+    } else {
+      setErrorHabilitacionConsultrio1(false);
+    }
+
+    if (validator.isEmpty(data.dirConsultorio2)) {
+      validationOk = false;
+      setErrorDirConsultorio2(true);
+    } else {
+      setErrorDirConsultorio2(false);
+    }
+    if (validator.isEmpty(data.fantasiaConsultorio2)) {
+      validationOk = false;
+      setErrorFantasiaConsultrio2(true);
+    } else {
+      setErrorFantasiaConsultrio2(false);
+    }
+    if (!validator.isAlphanumeric(data.habilitacionConsultorio2)) {
+      validationOk = false;
+      setErrorHabilitacionConsultrio2(true);
+    } else {
+      setErrorHabilitacionConsultrio2(false);
+    }
+    if (!validator.isNumeric(data.telFijoLaboral)) {
+      validationOk = false;
+      setErrorTelFijoLaboral(true);
+    } else {
+      setErrorTelFijoLaboral(false);
+    }
+    if (!validator.isNumeric(data.telCelularLaboral)) {
+      validationOk = false;
+      setErrorTelCelularLaboral(true);
+    } else {
+      setErrorTelCelularLaboral(false);
+    }
+    if (!validator.isAlphanumeric(data.facebook)) {
+      validationOk = false;
+      setErrorFacebook(true);
+    } else {
+      setErrorFacebook(false);
+    }
+    if (!validator.isAlphanumeric(data.instagram)) {
+      validationOk = false;
+      setErrorInstagram(true);
+    } else {
+      setErrorInstagram(false);
+    }
+    if (!validator.isAlphanumeric(data.twitter)) {
+      validationOk = false;
+      setErrorTwitter(true);
+    } else {
+      setErrorTwitter(false);
+    }
+    if (!validator.isAlphanumeric(data.web)) {
+      validationOk = false;
+      setErrorWeb(true);
+    } else {
+      setErrorWeb(false);
+    }
+    if (!validator.isAlphanumeric(data.tiktok)) {
+      validationOk = false;
+      setErrorTikTok(true);
+    } else {
+      setErrorTikTok(false);
     }
 
     if (validationOk) {
@@ -186,7 +327,6 @@ const Padron = () => {
             </Text>
             <FormHelperText marginBottom="4">Ejemplo: Perez</FormHelperText>
           </Box>
-
           <FormLabel htmlFor="number">DNI</FormLabel>
           <Input
             id="dni"
@@ -197,10 +337,13 @@ const Padron = () => {
                 dni: e.target.value,
               }))
             }
-          />
-          <FormHelperText marginBottom="4">
-            Ejemplo: 10200300 (sin puntos, solo números)
-          </FormHelperText>
+          />{" "}
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorDni && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: 10200300 (sin puntos, solo números)</FormHelperText>
+          </Box>
           <FormLabel htmlFor="number">CUIT</FormLabel>
           <Input
             id="cuit"
@@ -212,9 +355,14 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: 20102003002 (sin puntos ni guiones, solo números)
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorCuit && "Revise el campo"}
+            </Text>
+            <FormHelperText>
+              Ejemplo: 20102003002 (sin puntos ni guiones, solo números)
+            </FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Matrícula</FormLabel>
           <Input
             id="matricula"
@@ -226,9 +374,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: 1234 (sin puntos, solo números)
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorMatricula && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: 1234 (sin puntos, solo números)</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Sexo</FormLabel>
           <Input
             id="sexo"
@@ -240,9 +391,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            M:Masculino, F:Femenino, O:Otro
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorSexo && "Revise el campo"}
+            </Text>
+            <FormHelperText>M:Masculino, F:Femenino, N:No Binario</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Dirección</FormLabel>
           <Input
             id="direccion"
@@ -254,9 +408,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: Calle 123 Piso 1 Departamento 2
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorDireccion && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: Calle 123 Piso 1 Departamento 2</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Email Particular</FormLabel>
           <Input
             id="emailParticular"
@@ -268,7 +425,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">Ejemplo: juan@mail.com</FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorEmailPersonal && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: juan@mail.com</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Teléfono Fijo Particular</FormLabel>
           <Input
             id="telParticular"
@@ -280,9 +442,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: 38141231234 (no debe ingresar el 0)
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorTelFijoPersonal && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: 38141231234 (no debe ingresar el 0)</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">¿Está en relación de dependencia?</FormLabel>
           <Input
             id="relacionDeDependencia"
@@ -294,9 +459,14 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ingrese S si cobra con recibo de sueldo, N si no lo está.
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorRelacionDeDependencia && "Revise el campo"}
+            </Text>
+            <FormHelperText>
+              Ingrese S si cobra con recibo de sueldo, N si no lo está.
+            </FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">¿Es autónomo?</FormLabel>
           <Input
             id="esAutonomo"
@@ -308,9 +478,14 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ingrese S si es monotributista o responsable inscripto, N si no lo es.
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorEsAutonomo && "Revise el campo"}
+            </Text>
+            <FormHelperText>
+              Ingrese S si es monotributista o responsable inscripto, N si no lo es.
+            </FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">¿Tiene consultorio?</FormLabel>
           <Input
             id="tieneConsultorio"
@@ -322,9 +497,9 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ingrese S si tiene un consultorio, N si no.
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <FormHelperText>Ingrese S si tiene un consultorio, N si no.</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Dirección consultorio 1</FormLabel>
           <Input
             id="direccionConsultorio1"
@@ -336,9 +511,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: Calle 123 Piso 1 Departamento 2
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorDirConsultorio1 && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: Calle 123 Piso 1 Departamento 2</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Nombre de fantasía consultorio 1</FormLabel>
           <Input
             id="fantasiaConsultorio1"
@@ -350,7 +528,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">Ejemplo: Tu Odontólogo</FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorFantasiaConsultrio1 && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: Tu Odontólogo</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Número de habilitación consultorio 1</FormLabel>
           <Input
             id="habilitaciónConsultorio1"
@@ -362,7 +545,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">Ejemplo: 1234A</FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorHabilitacionConsultrio1 && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: 1234A</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Dirección consultorio 2</FormLabel>
           <Input
             id="direccionConsultorio2"
@@ -374,9 +562,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: Calle 123 Piso 1 Departamento 2
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorDirConsultorio2 && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: Calle 123 Piso 1 Departamento 2</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Nombre de fantasía consultorio 2</FormLabel>
           <Input
             id="fantasiaConsultorio2"
@@ -388,7 +579,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">Ejemplo: Tu Odontólogo</FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorFantasiaConsultorio2 && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: Tu Odontólogo</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Número de habilitación consultorio 2</FormLabel>
           <Input
             id="habilitaciónConsultorio2"
@@ -400,7 +596,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">Ejemplo: 1234A</FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorHabilitacionConsultrio2 && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: 1234A</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Teléfono laboral fijo</FormLabel>
           <Input
             id="telefonoLaboralFijo"
@@ -412,9 +613,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: 38141231234 (no debe ingresar el 0)
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorTelFijoLaboral && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: 38141231234 (no debe ingresar el 0)</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Teléfono laboral celular</FormLabel>
           <Input
             id="telefonoLaboralCelular"
@@ -426,9 +630,14 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: 38141231234 (no debe ingresar el 0 ni el 15)
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorTelCelularLaboral && "Revise el campo"}
+            </Text>
+            <FormHelperText>
+              Ejemplo: 38141231234 (no debe ingresar el 0 ni el 15)
+            </FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Facebook</FormLabel>
           <Input
             id="facebook"
@@ -440,9 +649,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: http://facebook.com/odontologo
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorFacebook && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: http://facebook.com/odontologo</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Instagram</FormLabel>
           <Input
             id="instagram"
@@ -454,7 +666,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">Ejemplo: @odontologo</FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorInstagram && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: @odontologo</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Twitter</FormLabel>
           <Input
             id="twitter"
@@ -466,7 +683,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">Ejemplo: @odontologo</FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorTwitter && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: @odontologo</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">Página web</FormLabel>
           <Input
             id="web"
@@ -478,9 +700,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">
-            Ejemplo: http://www.odontologo.com.ar
-          </FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorWeb && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: http://www.odontologo.com.ar</FormHelperText>
+          </Box>
           <FormLabel htmlFor="text">TikTok</FormLabel>
           <Input
             id="tiktok"
@@ -492,7 +717,12 @@ const Padron = () => {
               }))
             }
           />
-          <FormHelperText marginBottom="4">Ejemplo: odontologo</FormHelperText>
+          <Box marginBottom="4" marginTop="2">
+            <Text color="red.500" fontSize="sm">
+              {errorTikTok && "Revise el campo"}
+            </Text>
+            <FormHelperText>Ejemplo: odontologo</FormHelperText>
+          </Box>
         </FormControl>
         <Button
           colorScheme="teal"
